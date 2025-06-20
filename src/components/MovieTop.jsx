e8966e276c28e56b2fe2a72db2dc8227f97ebdf6
jjub0217 <noonssub1004@gmail.com>
2025-06-20 10:11:56 +0900

    rename: MoviePopular 컴포넌트 이름 수정

---
+++ src/components/MovieTop.jsx
@@ -0,0 +1,30 @@
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { MovieCard } from "./MovieCard";
+
+export function MovieTop({ topMovieList }) {
+  return (
+    <Swiper
+      loop={true}
+      centeredSlides={true}
+      spaceBetween={10}
+      modules={[Autoplay, Navigation, Pagination]}
+      slidesPerView="auto"
+      navigation={true}
+      pagination={{ type: "fraction" }}
+      // autoplay={{
+      //   delay: 5000, // 슬라이드 간 시간 (ms)
+      //   disableOnInteraction: false, // 사용자 터치 후에도 계속 자동재생
+      // }}
+    >
+      {topMovieList.slice(0, 10).map((el) => (
+        <SwiperSlide key={el.id}>
+          <MovieCard key={el.id} {...el} isSwiper={true} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+}
