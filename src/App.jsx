6856eb374705281841c753776cd946c0886cd42f
jjub0217 <noonssub1004@gmail.com>
2025-06-20 10:01:32 +0900

    docs: 스와이퍼에 내려주는 리스트 변경

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,43 @@ import "./App.scss";
 import Layout from "./components/Layout";
 import { Main } from "./components/Main";
 import { MovieDetail } from "./components/MovieDetail";
+import { MovieSearch } from "./components/MovieSearch";
 import { API_URL } from "./constant/imageBaseUrl";
 import useFetch from "./hooks/useFetch";
 
 function App() {
   const [popularList, setPopularList] = useState([]);
+  const [topMovieList, setTopMovieList] = useState([]);
 
-  const { results } = useFetch(
-    `${API_URL}/movie/popular?language=ko-KR&page=1`
+  const { data } = useFetch(`${API_URL}/movie/popular?language=ko-KR&page=1`);
+  const { data: topList } = useFetch(
+    `${API_URL}/movie/top_rated?language=en-US&page=1`
   );
 
   useEffect(() => {
-    if (results.length !== 0) {
-      const popularMovieList = results.filter((el) => el.adult === false);
-      setPopularList(popularMovieList);
-    }
-  }, [results]);
-
-  console.log(results);
+    if (!data || !data.results || !topList || !topList.results) return;
+    const popularMovieList = data.results.filter((el) => el.adult === false);
+    setPopularList(popularMovieList);
+    const topRatedMovieList = topList.results.filter(
+      (el) => el.adult === false
+    );
+    setTopMovieList(topRatedMovieList);
+  }, [data, topList]);
 
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Main popularList={popularList} />} />
-          {/* <Route path="/" element={<App IMAGE_BASE_URL={IMAGE_BASE_URL} />} /> */}
           <Route
-            path={`/detail/:id`}
-            element={<MovieDetail popularList={popularList} />}
+            index
+            element={
+              <Main popularList={popularList} topMovieList={topMovieList} />
+            }
+          />
+          <Route path={`/detail/:id`} element={<MovieDetail />} />
+          <Route
+            path={`/search`}
+            element={<MovieSearch popularList={popularList} />}
           />
         </Route>
       </Routes>
