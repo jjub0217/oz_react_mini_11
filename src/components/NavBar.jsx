51dbfc0063ef6ede39d2baaf0121c50b7c4da49a
jjub0217 <noonssub1004@gmail.com>
2025-06-20 10:05:39 +0900

    docs: useDebouce 기능을 활용하여 input 에 값을 입력할때마다 useDebounce 가 호출되도록 onChange 기능 수정

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,22 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import useDebounce from "../hooks/useDebounce";
 
 export default function NavBar() {
+  const [inputValue, setInputValue] = useState("");
+  const navigate = useNavigate();
+  const navigateDebounce = useDebounce();
+
+  const handleSearch = (e) => {
+    const searchValue = e.target.value;
+    setInputValue(searchValue);
+    if (searchValue.length === 0) {
+      navigate("/");
+    } else {
+      navigateDebounce(searchValue);
+    }
+  };
+
   return (
     <nav className="">
       <div className="inner flex h-[42px] justify-between items-center">
@@ -14,6 +30,8 @@ export default function NavBar() {
           <input
             type="text"
             name="search"
+            onChange={handleSearch}
+            value={inputValue}
             className="w-[100%] bg-transparent border-b-[1px] border-[#6201e0] outline-none text-[#fff] text-[12px] font-normal pb-[3px]"
           />
         </div>
