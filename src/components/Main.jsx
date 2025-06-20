388dbd56cf29ac5c7e5f0183365ab795f614c26c
jjub0217 <noonssub1004@gmail.com>
2025-06-20 15:22:22 +0900

    docs: 클래스이름 삭제 및 수정

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,15 +8,15 @@ export const Main = ({ popularList, topMovieList }) => {
         <p>영화 데이터를 불러오는 중입니다...</p>
       ) : (
         <>
-          <section className="section-popular">
+          <section className="movie-top">
             <MovieTop topMovieList={topMovieList} />
           </section>
-          <section>
+          <section className="">
             <div className="inner">
               <h2 className="text-[#fff] font-bold text-[1.1rem] mb-[10px] text-left">
                 새로 올라온 영화
               </h2>
-              <ul className="flex flex-wrap gap-[10px] justify-start">
+              <ul className="main-movie__list">
                 {popularList.map((el) => (
                   <MovieCard key={el.id} {...el} />
                 ))}
