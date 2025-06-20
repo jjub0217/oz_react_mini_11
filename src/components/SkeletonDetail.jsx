41c69bb4e160f6f3ed9741c30c42466aee42e5a9
jjub0217 <noonssub1004@gmail.com>
2025-06-20 10:03:20 +0900

    design: css 디자인 변경

--- src/components/SkeletonDetail.jsx
+++ src/components/SkeletonDetail.jsx
@@ -1,18 +1,18 @@
 export const SkeletonDetail = () => {
   return (
-    <section className="max-w-screen-lg h-[calc(100vh-60px)] justify-center text-[#fff] animate-pulse">
-      <div className="flex gap-[20px] w-[100%]">
-        <div className="pb-[calc((185/350)*100%)] w-[350px] bg-gray-700 rounded-lg" />
-        <div className="flex-1 flex flex-col gap-[20px]">
-          <div className="flex justify-between">
-            <div className="w-[60%] h-[24px] bg-gray-700 rounded" />
-            <div className="w-[20%] h-[18px] bg-gray-600 rounded" />
+    <section className="movie-detail skeleton">
+      <div className="inner">
+        <div className="movie-poster" />
+        <div className="movie-detail__info">
+          <div className="movie-detail__title-box">
+            <div className="movie-title" />
+            <div className="movie-detail__rating" />
           </div>
-          <div className="flex gap-[10px]">
-            <div className="w-[50px] h-[18px] bg-gray-600 rounded" />
-            <div className="w-[50px] h-[18px] bg-gray-600 rounded" />
+          <div className="movie-detail__genres">
+            <div className="movie-detail__genre" />
+            <div className="movie-detail__genre" />
           </div>
-          <div className="w-full h-[80px] bg-gray-800 rounded" />
+          <div className="movie-detail__overview" />
         </div>
       </div>
     </section>
