93405a588c41a26cb3c78ed84366623c93866f4f
jjub0217 <noonssub1004@gmail.com>
2025-06-23 10:18:15 +0900

    docs: 이미지 주소 수정

--- src/constant/imageBaseUrl.js
+++ src/constant/imageBaseUrl.js
@@ -1,3 +1,8 @@
-export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
+// export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
+
+export const IMAGE_BASE_URL = {
+  poster: "https://image.tmdb.org/t/p/w500", // 중간 해상도 포스터용
+  backdrop: "https://image.tmdb.org/t/p/original", // 고해상도 백드롭용
+};
 
 export const API_URL = "https://api.themoviedb.org/3";
