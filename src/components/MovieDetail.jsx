182e01a7511067109ab5833b36e089b23c22ee9e
jjub0217 <noonssub1004@gmail.com>
2025-06-23 10:09:14 +0900

    feat: 특정 영화의 리뷰 받아오는 기능을 추가하여 CommentContent 컴포넌트로 전달

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -1,34 +1,39 @@
 import { useState } from "react";
 import { useParams } from "react-router-dom";
+import { getTabs } from "../constant/getTabs";
 import { API_URL, IMAGE_BASE_URL } from "../constant/imageBaseUrl";
 import useFetch from "../hooks/useFetch";
+import { CommentContent } from "./CommentContent";
+import { MovieDetailTabs } from "./MovieDetailTab";
+import { RecommendContent } from "./RecommendContent";
 import { SkeletonDetail } from "./SkeletonDetail";
 
 export const MovieDetail = () => {
   const { id } = useParams();
   const { data: movieDetail } = useFetch(`${API_URL}/movie/${id}?language=ko`);
   const [isLoaded, setIsLoaded] = useState(false);
-  // const [activeTab, setActiveTab] = useState("comments");
+  const [activeTab, setActiveTab] = useState("comments");
 
-  // const { data: detailInfo } = useFetch(
-  //   `${API_URL}/movie/${id}/reviews?language=en-US&page=1`
-  // );
+  const { data: detailInfo } = useFetch(
+    `${API_URL}/movie/${id}/reviews?language=en-US&page=1`
+  );
 
-  // const commentCount = detailInfo?.results?.length ?? 0;
-  // const tabs = getTabs(commentCount);
+  const commentList = detailInfo?.results ?? [];
+  const commentCount = detailInfo?.results?.length ?? 0;
+  const tabs = getTabs(commentCount);
 
   if (!movieDetail) {
     return <SkeletonDetail />;
   }
 
   return (
-    <>
+    <div className="mt-[2rem]">
       <section className="movie-detail">
         <div className="inner">
           <div className="movie-poster">
             {!isLoaded && <p>🖼️ 이미지 로딩 중...</p>}
             <img
-              src={`${IMAGE_BASE_URL}${movieDetail.backdrop_path}`}
+              src={`${IMAGE_BASE_URL.backdrop}${movieDetail.backdrop_path}`}
               alt={movieDetail.title}
               onLoad={() => setIsLoaded(true)}
               style={{ display: isLoaded ? "block" : "none" }}
@@ -60,19 +65,26 @@ export const MovieDetail = () => {
           </div>
         </div>
       </section>
-      {/* <section className="movie-tab__info">
-        <div className="inner text-left w-full"> */}
-      {/* <MovieDetailTabs setActiveTab={setActiveTab} tabs={tabs} /> */}
-      {/* {activeTab === "comments" ? (
-            <CommentContent
-              detailInfo={detailInfo?.results}
-              commentCount={commentCount}
-            />
-          ) : activeTab === "recommend" ? (
-            <RecommendContent />
-          ) : null} */}
-      {/* </div>
-      </section> */}
-    </>
+      <section className="movie-tab__info pb-[80px] max-[768px]:px-[5vw]">
+        <div className="inner text-left w-full">
+          <MovieDetailTabs setActiveTab={setActiveTab} tabs={tabs} id={id} />
+          <div className="movie-tabs-content">
+            <div
+              style={{ display: activeTab === "comments" ? "block" : "none" }}
+            >
+              <CommentContent
+                detailInfo={commentList}
+                commentCount={commentCount}
+              />
+            </div>
+            <div
+              style={{ display: activeTab === "recommend" ? "block" : "none" }}
+            >
+              <RecommendContent id={id} />
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 };
