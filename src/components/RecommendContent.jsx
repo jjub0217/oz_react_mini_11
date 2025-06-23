2704303ccdc798a265aa3a629820827ae70f2d2c
jjub0217 <noonssub1004@gmail.com>
2025-06-23 10:10:07 +0900

    feat: MovieDetail 에서 추천 콘텐츠 데이터 받아서 RecommendContent 로 전달

---
+++ src/components/RecommendContent.jsx
@@ -0,0 +1,21 @@
+import { API_URL } from "../constant/imageBaseUrl";
+import useFetch from "../hooks/useFetch";
+import { MovieCard } from "./MovieCard";
+
+export const RecommendContent = ({ id }) => {
+  console.log(id);
+
+  const { data: recommendMovie } = useFetch(
+    `${API_URL}/movie/${id}/recommendations?language=ko&page=1`
+  );
+  console.log(recommendMovie);
+  const recommendList = recommendMovie?.results ?? [];
+
+  return (
+    <ul className="grid grid-cols-8 max-[768px]:grid-cols-4 gap-[10px] mt-[2rem]">
+      {recommendList.map((el) => (
+        <MovieCard {...el} key={el.id} />
+      ))}
+    </ul>
+  );
+};
