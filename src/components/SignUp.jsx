5d6d526ba53b694a9d30785615457aa969f1257c
jjub0217 <noonssub1004@gmail.com>
2025-06-24 23:53:19 +0900

    docs: fieldset 과 legend 추가, 필요없는 상태 제거

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -22,8 +22,6 @@ export const SignUp = () => {
     confirmPassword: "notMatch",
   });
 
-  const [signupError, setSignupError] = useState("");
-
   const changeValue = (e) => {
     const { name, value } = e.target;
     const nextUserInfo = { ...userInfo, [name]: value };
@@ -42,68 +40,72 @@ export const SignUp = () => {
     const { data, error } = await signUp({ email, password, name });
     console.log("✅ Supabase 응답:", data, error);
 
-    if (error) {
-      setSignupError(error.message); // ✅ 에러 표시용
-    } else {
+    if (!error) {
       alert("회원가입 요청이 완료되었습니다. 이메일을 확인해주세요.");
-      // 필요 시 로그인 페이지로 이동
     }
   };
 
   return (
-    <main>
-      <div className="inner max-w-screen-lg">
-        <h1>회원가입</h1>
+    <main className="h-[calc(80vh-74px)] flex items-center">
+      <div className="inner max-w-screen-md  w-full bg-[#252525] px-[50px] py-[50px]">
+        <h2 className="text-[2rem] mb-[30px]">회원가입</h2>
         <form ref={formRef} onSubmit={requestSignUp}>
-          <InputField
-            label="이메일"
-            name="email"
-            type="email"
-            placeholder="이메일 주소를 입력하세요"
-            value={userInfo.email}
-            onChange={changeValue}
-            errorMessage={getErrorMessage("signup", "email", errorType.email)}
-          />
-          <InputField
-            pageType="signup"
-            label="이름"
-            name="name"
-            type="text"
-            placeholder="2~8자, 숫자, 한글, 영어만 사용"
-            value={userInfo.name}
-            onChange={changeValue}
-            errorMessage={getErrorMessage("signup", "name", errorType.name)}
-          />
-          <InputField
-            pageType="signup"
-            label="비밀번호"
-            name="password"
-            type="password"
-            placeholder="영문 대문자/소문자 + 숫자의 조합 사용"
-            value={userInfo.password}
-            onChange={changeValue}
-            errorMessage={getErrorMessage(
-              "signup",
-              "password",
-              errorType.password
-            )}
-          />
-          <InputField
-            pageType="signup"
-            label="비밀번호 확인"
-            name="confirmPassword"
-            type="password"
-            placeholder="비밀번호를 다시 입력하세요"
-            value={userInfo.confirmPassword}
-            onChange={changeValue}
-            errorMessage={getErrorMessage(
-              "signup",
-              "confirmPassword",
-              errorType.confirmPassword
-            )}
-          />
-          {signupError && <p className="text-[red] text-sm">{signupError}</p>}
-          <button type="submit">회원가입</button>
+          <fieldset>
+            <legend className="blind">회원가입 폼</legend>
+            <InputField
+              label="이메일"
+              name="email"
+              type="email"
+              placeholder="이메일 주소를 입력하세요"
+              value={userInfo.email}
+              onChange={changeValue}
+              errorMessage={getErrorMessage("signup", "email", errorType.email)}
+            />
+            <InputField
+              pageType="signup"
+              label="이름"
+              name="name"
+              type="text"
+              placeholder="2~8자, 숫자, 한글, 영어만 사용"
+              value={userInfo.name}
+              onChange={changeValue}
+              errorMessage={getErrorMessage("signup", "name", errorType.name)}
+            />
+            <InputField
+              pageType="signup"
+              label="비밀번호"
+              name="password"
+              type="password"
+              placeholder="영문 대문자/소문자 + 숫자의 조합 사용"
+              value={userInfo.password}
+              onChange={changeValue}
+              errorMessage={getErrorMessage(
+                "signup",
+                "password",
+                errorType.password
+              )}
+            />
+            <InputField
+              pageType="signup"
+              label="비밀번호 확인"
+              name="confirmPassword"
+              type="password"
+              placeholder="비밀번호를 다시 입력하세요"
+              value={userInfo.confirmPassword}
+              onChange={changeValue}
+              errorMessage={getErrorMessage(
+                "signup",
+                "confirmPassword",
+                errorType.confirmPassword
+              )}
+            />
+          </fieldset>
+          <button
+            type="submit"
+            className="p-[20px] mt-[25px] bg-[#6201e0] text-[#fff] w-full rounded-[30px] text-[1.2rem]"
+          >
+            회원가입
+          </button>
         </form>
       </div>
     </main>
