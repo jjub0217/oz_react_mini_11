0773a3f7c6a0d49e24ee46cd2f2ba92830a3f4b0
jjub0217 <noonssub1004@gmail.com>
2025-06-24 19:24:04 +0900

    docs: client import

---
+++ src/context/SupabaseContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useContext, useState } from "react";
+import { supabaseClient } from "../supabaseClient";
+
+const SUPABASE = createContext(null);
+
+export const SupabaseProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  return (
+    <SUPABASE.Provider value={{ supabase: supabaseClient, user, setUser }}>
+      {children}
+    </SUPABASE.Provider>
+  );
+};
+
+export const useSupabase = () => {
+  const supabase = useContext(SUPABASE);
+
+  if (!supabase) {
+    throw new Error("Supabase가 초기화되지 않았습니다.");
+  }
+
+  return supabase;
+};
