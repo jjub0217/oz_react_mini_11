cf58a7bc76a765ec5b498999277c05aaa0998fe1
jjub0217 <noonssub1004@gmail.com>
2025-06-24 14:19:10 +0900

    feat: 로그인 페이지 구현

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,104 @@
+import { useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+import { useOAuth } from "../hooks/useOAuth";
+import { getErrorMessage, validateField } from "../utils/validation";
+import { InputField } from "./InputField";
+
 export const Login = () => {
+  const loginForm = useRef();
+  const emailInput = useRef(null);
+
+  const [loginUserInfo, setLoginUserInfo] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [errorType, setErrorType] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [signupError, setSignupError] = useState("");
+
+  const { Login, isLoading, authError } = useAuth();
+  const { loginWithKakao, loginWithGoogle } = useOAuth();
+
+  const requestLogin = async (e) => {
+    e.preventDefault();
+    const isValid = Object.values(errorType).every((val) => val === "");
+    if (!isValid) return;
+    const { email, password } = loginUserInfo;
+    const { data, error } = await Login({ email, password });
+    console.log("✅ 로그인 응답:", data, error);
+    if (error) {
+      setSignupError(error.message);
+    } else {
+      alert("로그인이 완료되었습니다");
+    }
+  };
+
+  const changeValue = (e) => {
+    const { name, value } = e.target;
+    const nextUserInfo = { ...loginUserInfo, [name]: value };
+    setLoginUserInfo(nextUserInfo);
+    const error = validateField(name, value, nextUserInfo);
+    setErrorType((prev) => ({ ...prev, [name]: error }));
+  };
+
   return (
-    <>
-      <div></div>
-    </>
+    <main>
+      <div className="inner max-w-screen-lg">
+        <h1>로그인</h1>
+        <form action="" method="post" ref={loginForm} onSubmit={requestLogin}>
+          <InputField
+            label="이메일"
+            name="email"
+            type="email"
+            placeholder="이메일 주소를 입력하세요"
+            value={loginUserInfo.email}
+            onChange={changeValue}
+            errorMessage={getErrorMessage("login", "email", errorType.email)}
+          />
+          <InputField
+            label="비밀번호"
+            name="password"
+            type="password"
+            placeholder="비밀번호를 입력하세요"
+            value={loginUserInfo.password}
+            onChange={changeValue}
+            errorMessage={getErrorMessage(
+              "login",
+              "password",
+              errorType.password
+            )}
+          />
+          {signupError && <p className="text-[red] text-sm">{signupError}</p>}
+          <button>로그인</button>
+          <div>
+            <p>OZ무비가 처음이신가요?</p>
+            <Link to={`/signup`}>간편가입</Link>
+          </div>
+          <p>또는 다른 서비스 계정으로 로그인</p>
+          <button
+            type="button"
+            onClick={() =>
+              loginWithKakao("http://localhost:5173/oauth/callback")
+            }
+          >
+            카카오로 로그인
+          </button>
+          <button
+            type="button"
+            onClick={() =>
+              loginWithGoogle("http://localhost:5173/oauth/callback")
+            }
+          >
+            Google로 로그인
+          </button>
+        </form>
+        {/* </form> */}
+      </div>
+    </main>
   );
 };
