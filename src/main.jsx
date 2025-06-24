12106be66960228f9a16bd9c377decf3723ceeab
jjub0217 <noonssub1004@gmail.com>
2025-06-24 19:02:09 +0900

    docs: supabase Provider 삽입

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { SupabaseProvider } from "./context/SupabaseContext";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <SupabaseProvider>
+      <App />
+    </SupabaseProvider>
   </BrowserRouter>
 );
