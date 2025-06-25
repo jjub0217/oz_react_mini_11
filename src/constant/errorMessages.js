d21b2adaff4be366f49c6f13c1986e3c711b53f2
jjub0217 <noonssub1004@gmail.com>
2025-06-25 16:25:46 +0900

    docs: 에러메세지 수정

--- src/constant/errorMessages.js
+++ src/constant/errorMessages.js
@@ -8,24 +8,24 @@ export const ERROR_MESSAGES = {
     password: {
       noText: "비밀번호를 입력해 주세요.",
       blankInText: "비밀번호에 공백이 포함되어 있습니다.",
-      tooShort: "비밀번호는 8자 이상이어야 합니다.",
+      invalidPassword: "영어 대문자/소문자 + 숫자의 조합으로 사용해야 합니다.",
     },
   },
   signup: {
     email: {
       noText: "이메일을 입력해 주세요.",
       blankInText: "이메일에 공백이 포함되어 있습니다.",
-      wrongEmail: "이메일 형식으로 입력해주세요.",
+      wrongEmail: "올바른 이메일 양식으로 입력해주세요.",
     },
     name: {
       noText: "이름을 입력해주세요.",
       blankInText: "이름에 공백이 포함되어 있습니다.",
-      wrongLength: "이름은 2자 이상 8자 이하로 입력해주세요.",
+      invalidName: "이름은 2~8자 사이 숫자, 한글, 영어만 입력해주세요.",
     },
     password: {
       noText: "필수항목입니다.",
       blankInText: "비밀번호에 공백이 포함되어 있습니다.",
-      tooShort: "비밀번호는 8자 이상이어야 합니다.",
+      invalidPassword: "영어 대문자/소문자 + 숫자의 조합으로 사용해야 합니다.",
     },
     confirmPassword: {
       notMatch: "비밀번호가 일치하지 않습니다.",
