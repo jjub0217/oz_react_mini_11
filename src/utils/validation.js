f3ab82325f55d17f2427e3831a6298fbd125926d
jjub0217 <noonssub1004@gmail.com>
2025-06-25 16:23:28 +0900

    feat: 비밀번호와 이름의 유효성 검증 조건 기능 추가 구현

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -6,9 +6,22 @@ export const validateField = (name, value, userInfo) => {
     if (value === "") return "noText";
     if (/\s/.test(value)) return "blankInText";
     if (name === "email" && !value.includes("@")) return "wrongEmail";
-    if (name === "password" && value.length < 8) return "tooShort";
-    if (name === "name" && (value.length < 2 || value.length > 8))
-      return "wrongLength";
+    if (name === "password") {
+      const onlyAllowedChars = /^[a-zA-Z0-9]+$/.test(value); // 허용된 문자만 포함
+      const hasUpper = /[A-Z]/.test(value); // 대문자 포함
+      const hasLower = /[a-z]/.test(value); // 소문자 포함
+      const hasNumber = /[0-9]/.test(value); // 숫자 포함
+
+      if (!onlyAllowedChars || !hasUpper || !hasLower || !hasNumber) {
+        return "invalidPassword";
+      }
+    }
+    if (name === "name") {
+      const isWrongLength = value.length < 2 || value.length > 8;
+      const hasInvalidChar = !/^[0-9a-zA-Z가-힣]+$/.test(value);
+
+      if (isWrongLength || hasInvalidChar) return "invalidName";
+    }
 
     return "";
   }
