1c6df28a3733c0b4d83ac73212c666f3d1a7ae71
jjub0217 <noonssub1004@gmail.com>
2025-06-26 21:20:46 +0900

    feat: InputField 컴포넌트 내부에서, 파일 업로드 하는 input 분기

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -10,23 +10,38 @@ export const InputField = ({
   errorMessage,
 }) => {
   return (
-    <div className="flex flex-col items-start w-full ">
+    <div
+      className={`flex flex-col items-start ${
+        type === "file" ? "w-fit" : "w-full"
+      }`}
+    >
       <label htmlFor={name} className="blind">
         {label}
       </label>
-      <input
-        id={name}
-        name={name}
-        type={type}
-        value={value}
-        onChange={onChange}
-        placeholder={placeholder}
-        className="text-[#fff] w-full bg-[#2f2f2f] pl-[20px] 
+      {type === "file" ? (
+        <input
+          id={name}
+          name={name}
+          type="file"
+          accept="image/*"
+          onChange={onChange}
+          className="text-[#fff] bg-[#2f2f2f] pl-[20px] py-[15px] rounded-[10px] file:cursor-pointer file:bg-[#6201e0] file:text-white file:px-[10px] file:py-[5px] file:rounded-[6px]"
+        />
+      ) : (
+        <input
+          id={name}
+          name={name}
+          type={type}
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+          className="text-[#fff] w-full bg-[#2f2f2f] pl-[20px] 
         py-[20px] placeholder:text-[#888] outline-0
         focus:bg-[#fff]  focus:text-[#252525] rounded-[10px]
         "
-        autoComplete="on"
-      />
+          autoComplete="on"
+        />
+      )}
       <span className="w-full text-left min-h-[1.5rem] block">
         <Strong>{errorMessage}</Strong>
       </span>
