0c86f8d60b590d346549e268ff3bb2ae71061d90
jjub0217 <noonssub1004@gmail.com>
2025-06-30 13:11:37 +0900

    feat: 탭을 두곳에서 사용하고 있는데, 마이페이지에서 활용될 경우에 주소 쿼리 보낼수 있도록 수정

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,20 +1,42 @@
+import { useNavigate } from "react-router-dom";
+
 export const Tabs = ({ tabs, setActiveTab, pageType }) => {
+  const navigate = useNavigate();
   const handleTab = (key) => {
-    setActiveTab(key);
+    const tabKey = key;
+
+    if (pageType === "mypage") {
+      if (tabKey === "review") {
+        navigate("/my-page/reviews");
+      } else if (tabKey === "favorites") {
+        navigate("/my-page/favorites");
+      } else if (tabKey === "profile") {
+        navigate("/my-page/profile");
+      } else {
+        navigate("/my-page");
+      }
+    } else {
+      setActiveTab(tabKey);
+    }
+    console.log(tabKey);
   };
 
   return (
     <div
       className={`flex ${
-        pageType === "mypage" && "flex-col p-0 h-[100%] border-b-0"
-      } gap-[20px] pb-[1.4rem] border-b-[1px] max-[768px]:pb-[1rem]`}
-      style={{ borderColor: "rgba(255, 255, 255, 0.2)" }}
+        pageType === "mypage" && "flex-col p-0 h-[100%] pb-0"
+      } gap-[20px] pb-[1.4rem] border-b-[1px] max-[1700px]:text-[1rem] max-[768px]:pb-[1rem]`}
+      style={{
+        borderColor:
+          pageType === "mypage" ? "transparent" : "rgba(255, 255, 255, 0.2)",
+        border: pageType === "mypage" ? "0" : "auto",
+      }}
     >
       {tabs.map((tab) => (
         <button
           key={tab.key}
           onClick={() => handleTab(tab.key)}
-          className="text-[1.4rem] max-[768px]:text-[1rem]"
+          className="text-[1.4rem] max-[1700px]:text-[1rem] max-[768px]:text-[1rem]"
         >
           {tab.label}
         </button>
