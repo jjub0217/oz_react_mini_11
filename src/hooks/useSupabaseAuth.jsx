13a946d8b6827e0dae3c5836d7dd11c3b7a53c97
jjub0217 <noonssub1004@gmail.com>
2025-06-30 13:10:59 +0900

    docs: props 전달

--- src/hooks/useSupabaseAuth.jsx
+++ src/hooks/useSupabaseAuth.jsx
@@ -2,7 +2,16 @@ import { useAuth } from "./useAuth";
 import { useOAuth } from "./useOAuth";
 
 export const useSupabaseAuth = () => {
-  const { signUp, login, logout, getUserInfo, user } = useAuth();
+  const {
+    signUp,
+    login,
+    logout,
+    getUserInfo,
+    updateNickname,
+    updateProfileImage,
+    updateBackgroundImage,
+    user,
+  } = useAuth();
   const { loginWithGoogle, loginWithKakao } = useOAuth();
 
   return {
@@ -10,6 +19,9 @@ export const useSupabaseAuth = () => {
     signUp,
     logout,
     getUserInfo,
+    updateNickname,
+    updateProfileImage,
+    updateBackgroundImage,
     user,
     loginWithKakao,
     loginWithGoogle,
